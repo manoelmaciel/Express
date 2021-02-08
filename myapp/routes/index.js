5c252055062f7807ea33b5676e9ef5c35a78e061
manoelmaciel <manoelbraz.maciel@gmail.com>
2021-02-08 04:34:23 -0300

    Testes ... Só mais um teste!

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,38 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(req, res, next) {
-  res.send('Got a POST ... Request!')
-});
-
-router.put('/', function(req, res, next) {
-  res.send('Got a PUT ... Request!')
-});
-
-router.patch('/', function(req, res, next) {
-  res.send('Got a PATH ... Request!')
-});
-
-router.delete('/', function(req, res, next) {
-  res.send('Got a DELETE ... Request!')
-});
-
-router.copy('/', function(req, res, next) {
-  res.send('Got a COPY ... Request!')
-});
-
-router.head('/', function(req, res, next) {
-  res.send('Got a HEAD ... Request!')
-});
-
-router.options('/', function(req, res, next) {
-  res.send('Got a OPTIONS ... Request!')
-});
-
-router.link('/', function(req, res, next) {
-  res.send('Got a LINK ... Request!')
-});
-
-// Todas as rotas funcionaram no POSTMAN
-
 module.exports = router;
--- myapp/routes/users.js
+++ myapp/routes/users.js
@@ -6,30 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('Respond with a resource ... Users!');
 });
 
-router.unlink('/', function(req, res, next) {
-  res.send('Got a UNLINK ... Request!');
-});
-
-router.purge('/', function(req, res, next) {
-  res.send('Got a PURGE ... Request!');
-});
-
-router.lock('/', function(req, res, next) {
-  res.send('Got a LOCK ... Request!');
-});
-
-router.unlock('/', function(req, res, next) {
-  res.send('Got a UNLOCK ... Request!');
-});
-
-router.propfind('/', function(req, res, next) {
-  res.send('Got a PROPFIND ... Request!');
-});
-
-// router.view('/', function(req, res, next) {
-//   res.send('Got a VIEW ... Request!');
-// }); 
-
-// A rota View ... Não é definida!
-
 module.exports = router;
