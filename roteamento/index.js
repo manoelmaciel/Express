3314fb5ad478c545e5488289f7e611ad1ae400be
manoelmaciel <manoelbraz.maciel@gmail.com>
2021-02-09 02:46:55 -0300

    Roteamento ... Hello World!

--- roteamento/index.js
+++ roteamento/index.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
 
+// respond with "Hello World!" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   res.send('Hello ... World!')
 })
 
-app.post('/', (req, res) => {
-  res.send('Got a POST request')
-})
+// app.post('/', (req, res) => {
+//   res.send('Got a POST request')
+// })
 
-app.put('/user', (req, res) => {
-  res.send('Got a PUT request at /user')
-})
+// app.put('/user', (req, res) => {
+//   res.send('Got a PUT request at /user')
+// })
 
-app.delete('/user', (req, res) => {
-  res.send('Got a DELETE request at /user')
-})
+// app.delete('/user', (req, res) => {
+//   res.send('Got a DELETE request at /user')
+// })
 
 app.listen(3000)
